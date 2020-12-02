aac44dde930069b041c41323942ec79f0cefd7fa
Sebastian Andreasson <xandse@ml-200403-001.local>
2020-12-02 14:17:01 +0100

    fetch images from api and list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,51 @@
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
+
+const { REACT_APP_API_URL } = process.env
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`
+
+const Image = styled.div`
+  border: 1px solid black;
+  padding: 10px;
+  margin: 10px;
+
+  display: flex;
+  flex-direction: column;
+
+  > img {
+    width: 200px;
+    height: 100px;
+  }
+`
+
 function App() {
-  return <div className="App">hejsansvejsan</div>
+  const [images, setImages] = useState([])
+  useEffect(() => {
+    const getImages = async () => {
+      const response = await axios.get(`${REACT_APP_API_URL}/images`)
+      setImages(response.data)
+    }
+    getImages()
+  }, [])
+
+  return (
+    <div>
+      <Container>
+        {images.map((image) => (
+          <Image>
+            <span>{image.imageTaken}</span>
+            <img src={image.imageUrl}></img>
+          </Image>
+        ))}
+      </Container>
+    </div>
+  )
 }
 
 export default App
