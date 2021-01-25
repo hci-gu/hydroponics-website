f03ee923f2388fe15594275c838c0de1fe43a661
Sebastian Andreasson <xandse@ml-200403-001.local>
2021-01-25 11:50:01 +0100

    inprogress crop for imagedisplayer

--- src/components/ImageDisplayer.js
+++ src/components/ImageDisplayer.js
@@ -1,12 +1,30 @@
 import { Card, Image } from 'antd'
 import moment from 'moment'
+import styled from 'styled-components'
+
+const IMAGE_WIDTH = 2000
+const PLANT_WIDTH = 400
+
+const ImageCropper = styled.div`
+  width: ${PLANT_WIDTH}px;
+  height: 300px;
+  overflow: hidden;
+`
 
 const Plant = ({ plant, images }) => {
+  if (images.length === 0) return <div>empty</div>
+
+  const imageStyle = {
+    marginTop: 25,
+    marginLeft: -(PLANT_WIDTH * plant.imagePosition) + 50,
+    width: IMAGE_WIDTH,
+  }
+
   return (
     <Card title={`${plant.name} - ${plant.id}`}>
-      {images.length > 0 && (
-        <Image width={'100%'} src={images[images.length - 1].imageUrl} />
-      )}
+      <ImageCropper>
+        <Image style={imageStyle} src={images[0].imageUrl} />
+      </ImageCropper>
     </Card>
   )
 }
