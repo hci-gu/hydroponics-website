76b91c3df8abe1c79619ecbc033b621b0ab93d96
Sebastian Andreasson <sebastian.andreasson93@gmail.com>
2021-03-26 15:37:55 +0100

    add back imageCache

--- src/components/ImageDisplayer.js
+++ src/components/ImageDisplayer.js
@@ -66,18 +66,35 @@ const PlantImages = ({ plant }) => {
     width: IMAGE_WIDTH,
   }
 
+  useEffect(() => {
+    const from = Math.max(0, index - 10)
+    const to = Math.min(images.length, index + 10)
 
-  if (images.length === 0) return <Card
-  hoverable
-  style={{ width: `${CARD_WIDTH}px`, textAlign: 'center' }}
-  title={`Den här plantan är påväg!`}
->
-  <ImageCropper>
-    <AntImage style={{width: 260, top: 160, left: 225}} src= 'https://i.imgur.com/1KmQLKx.png' preview={false}/>
-  </ImageCropper>
-  
-</Card>
+    for (let i = from; i < to; i++) {
+      if (!imageCache[i]) {
+        const preloadImage = new Image()
+        preloadImage.src = images[i].imageUrl
+        imageCache[i] = preloadImage
+      }
+    }
+  }, [images, index])
 
+  if (images.length === 0)
+    return (
+      <Card
+        hoverable
+        style={{ width: `${CARD_WIDTH}px`, textAlign: 'center' }}
+        title={`Den här plantan är påväg!`}
+      >
+        <ImageCropper>
+          <AntImage
+            style={{ width: 260, top: 160, left: 225 }}
+            src="https://i.imgur.com/1KmQLKx.png"
+            preview={false}
+          />
+        </ImageCropper>
+      </Card>
+    )
 
   const imageToDisplay = images[index]
   return (
