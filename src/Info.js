59f13bc8e5dd91ef7007f5aec3aa4a908c03f783
AxelFilipsson <filipsson960409@gmail.com>
2021-04-12 19:18:41 +0200

    text changes

--- src/Info.js
+++ src/Info.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import { useRecoilValue } from 'recoil'
-import { plantsAtom } from './state'
 import { Typography, Divider } from 'antd';
 const { Title, Paragraph, Text, Link } = Typography;
 
@@ -15,54 +13,67 @@ background-color: #FAFAFA;
 padding-left: 10%;
 padding-right: 10%;
 padding-top: 5%;
-
 `
 
 
-const TextBox = styled.div`
-
-
-  // background-color: yellow;
- 
+const TextBox = styled.div` 
 `
 
 function App() {
   
   return (
   <Root>
-<TextBox>
-    <Typography>
-      <Title level={3}>Om projektet</Title>
-        <Paragraph>
-        Det här projektet utforskar metoder för att studera och kontrollera växter med hjälp av digital teknik. Resultaten från forskningen kommer användas i forskning inom människa-dator interaktion. Projektet är i synnerhet intresserad utav växtodling ur ett visuellt perspektiv.
-      </Paragraph>
-      <Title level={3}>Metod</Title>
-        <Paragraph>
-          Detta projekt studerar hydroponiska odlingar där vi använder oss av Deep water culture(DWC)metoden.
-        </Paragraph>
-        <Title level={3}>Information om planteringarna</Title>
-        <Paragraph>
-        <strong>Om inget annat nämns för de unika plantorna så ges växterna: </strong>   
-        <br></br>
-        2ml pH-down
-        <br></br>
-        15ml grow
-        <br></br>
-        10ml micro
-        <br></br>
-        5ml bloom.
-        <br></br>
-        7l vatten
-        </Paragraph>
-        <Title level={3}>Utrustning</Title>
-        <Paragraph>
-        <strong>Växtbelysning:</strong> Sansi LED Växtlampa E27 - 15W 
-        <br></br>
-        <strong>Luftpump:</strong> BOYU S-2000 Syrepump 480l/tim
-        </Paragraph>
-    </Typography>
-    </TextBox>
-    </Root>
+    <TextBox>
+        <Typography>
+          <Title level={3}>Om projektet</Title>
+            <Paragraph>
+            Det här projektet utforskar metoder för att studera och kontrollera växter med hjälp av digital teknik. Resultaten från forskningen kommer användas i forskning inom människa-dator interaktion. Projektet fokuserar i synnerhet på växtodling ur ett visuellt perspektiv.
+          </Paragraph>
+            <Title level={3}>Information om odlningen</Title>
+            <Paragraph>
+            <strong>Om inget annat nämns för de unika plantorna så ges växterna: </strong>   
+            <br></br>
+            2ml pH-down
+            <br></br>
+            15ml grow
+            <br></br>
+            10ml micro
+            <br></br>
+            5ml bloom.
+            <br></br>
+            7l vatten
+            <br></br>
+            <br></br>
+            <strong>Övrig information </strong>   
+            <br></br>
+            Växterna odlas med hjälp av DWC(Deep Water Culture)metoden.
+            <br></br>
+            Odlingsmedierna(stenull) doppas i 1ml Root!t First Feed blandat med 2dl vatten innan frösättning. 
+            <br></br>
+            Luftpumpen är på lägsta styrka under hela växtfasen.
+            <br></br>
+            Växtbelysningen placeras 40cm ovanför växtbehållarnas lock.
+            <br></br>
+            Växtbelysningen placeras 20cm ovanför sticklingbehållarens golv. 
+            <br></br>
+            
+            
+            </Paragraph>
+            <Title level={3}>Utrustning</Title>
+            <Paragraph>
+            <strong>Växtbelysning:</strong> Sansi LED Växtlampa E27 - 15W 
+            <br></br>
+            <strong>Luftpump:</strong> BOYU S-2000 Syrepump 480l/tim
+            <br></br>
+            <strong>Näring sticklingar</strong> Root!t First Feed
+            <br></br>
+            <strong>Näring plantor</strong> GHE FloraBloom, FloraMicro, FloraGro.
+            <br></br>
+            <strong>pH-reglerare</strong> Vitalink ESSENTIALS pH Down EASY och pH Up EASY        
+            </Paragraph>
+        </Typography>
+      </TextBox>
+  </Root>
   )
 }
 
